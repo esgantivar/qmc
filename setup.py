7c13de56df5e87cc4f9d1c4ff12b5030099fcc28
Sneyder Gantiva <esgantivar@unal.edu.co>
2021-03-07 17:16:49 -0500

    update dependencies

--- setup.py
+++ setup.py
@@ -13,6 +13,6 @@ setuptools.setup(
     author="Fabio A. González et al., Sneyder Gantiva",
     author_email="fagonzalezo@unal.edu.co, esgantivar@unal.edu.co",
     license="GNUv3",
-    install_requires=["scipy", "numpy >= 1.19.2", "scikit-learn", "tensorflow >= 2.2.0", "typeguard", "tensornetworks"],
+    install_requires=["scipy", "numpy >= 1.19.2", "scikit-learn", "tensorflow >= 2.2.0", "typeguard", "tensornetwork"],
     python_requires='>=3.6'
 )
